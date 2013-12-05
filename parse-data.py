b31ef2299a5c0ddefb586883eb5fce2ab779ddb0
ebtre <antti.e.junkkari@gmail.com>
2013-12-05 20:32:58 +0200

    New version of parse-data

--- parse-data.py
+++ parse-data.py
@@ -1,3 +1,9 @@
-with open('derp.html') as page:
+from bs4 import BeautifulSoup
+
+with open('input.html') as page:
 	file_content = page.read()
-	print(file_content)
+	# print(file_content)
+
+soup = BeautifulSoup(file_content)
+
+print(soup.title)
