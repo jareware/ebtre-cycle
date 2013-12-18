fc5c1b11f0ed46ea41b826c87c4020e31fc3b309
ebtre <antti.e.junkkari@gmail.com>
2013-12-18 19:55:22 +0200

    Create new file to fetch from both Wiggle and Bike-discount, run parse after

--- fetch-data.sh
+++ fetch-data.sh
@@ -4,5 +4,5 @@ if [ -z "$1" ]; then
 	echo "No url specified"
 	exit 1
 fi
-   
+
 curl --user-agent "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)" "$1"
--- parse-data.py
+++ parse-data.py
@@ -10,3 +10,15 @@ soup = BeautifulSoup(file_content)
 price = soup.select(".unit-price")[0]
 
 print(price.get_text())
+
+with open('input2.html') as page2:
+        file_content = page2.read()
+        # print(file_content)
+
+soup = BeautifulSoup(file_content)
+
+# price = soup.find(class="unit-price")
+price2 = soup.select(".price")[0]
+
+print(price2.get_text())
+
